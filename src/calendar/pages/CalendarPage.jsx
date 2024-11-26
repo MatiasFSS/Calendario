872f137333aba592e682708de4f056f08958f420
Matías Fabian Sánchez Soto <matias.fss97@gmail.com>
2024-11-26 11:16:19 -0300

    Escuchar eventos del calendario y mantener vista donde quedo la ultima vez

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -6,6 +6,7 @@ import { addHours} from 'date-fns'
 import { Navbar } from "../components/Navbar"
 import { localizer, getMessagesEs } from '../../helpers'
 import { CalendarEvent } from '../components/CalendarEvent'
+import { useState } from 'react'
 
 
 const events = [{
@@ -22,6 +23,9 @@ const events = [{
 }]
 
 export const CalendarPage = () => {
+
+  const [lastView, setLastView] = useState(localStorage.getItem('lastView') || 'week');
+  
 
   const eventStyleGetter = ( event, start, end, isSelected) => {
 
@@ -40,6 +44,19 @@ export const CalendarPage = () => {
 
   }
 
+  const onDoubleClick = ( event ) => {
+      console.log({doubleClick:event})
+  }
+
+  const onSelect = ( event ) => {
+    console.log({click:event})
+  }
+
+  const onViewChanged = ( event ) => {
+    localStorage.setItem('lastView', event)
+    setLastView(event)
+  }
+
   return (
     <>
       <Navbar/>
@@ -48,6 +65,7 @@ export const CalendarPage = () => {
         culture='es'
         localizer={localizer}
         events={events}
+        defaultView={lastView}
         startAccessor="start"
         endAccessor="end"
         style={{ height: 'calc(100vh - 80px)' }}
@@ -56,6 +74,9 @@ export const CalendarPage = () => {
         components={{
           event:CalendarEvent
         }}
+        onDoubleClickEvent={onDoubleClick}
+        onSelectEvent={onSelect}
+        onView={onViewChanged}
       />
 
     </>
