15dc85dd4e6997ef05c0f9e17783f8c8ab65b4b2
Matías Fabian Sánchez Soto <matias.fss97@gmail.com>
2024-11-25 22:53:43 -0300

    estructurar de mejor forma el codigo del calendario

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,24 +1,12 @@
-import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
+import { Calendar } from 'react-big-calendar'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
 
-import enUS from 'date-fns/locale/en-US'
-import { addHours, parse, startOfWeek, getDay, format } from 'date-fns'
+
+import { addHours} from 'date-fns'
 
 import { Navbar } from "../components/Navbar"
+import { localizer, getMessagesEs } from '../../helpers'
 
-
-
-const locales = {
-  'en-US': enUS,
-}
-
-const localizer = dateFnsLocalizer({
-  format,
-  parse,
-  startOfWeek,
-  getDay,
-  locales,
-})
 
 const events = [{
   title: 'Cumpleaños del jefe',
@@ -34,16 +22,37 @@ const events = [{
 }]
 
 export const CalendarPage = () => {
+
+  const eventStyleGetter = ( event, start, end, isSelected) => {
+
+    console.log({event, start, end, isSelected})
+
+    const style = {
+      backgroundColor: '#347CF7',
+      borderRadius: '0px',
+      opacity: 0.8,
+      color:'white'
+    }
+
+    return {
+      style
+    }
+
+  }
+
   return (
     <>
       <Navbar/>
       
       <Calendar
-      localizer={localizer}
-      events={events}
-      startAccessor="start"
-      endAccessor="end"
-      style={{ height: 'calc(100vh - 80px)' }}
+        culture='es'
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 'calc(100vh - 80px)' }}
+        messages={getMessagesEs()}
+        eventPropGetter={eventStyleGetter}
       />
 
     </>
