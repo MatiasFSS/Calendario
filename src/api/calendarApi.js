ffab856a175e3aa3998995486a8e12ffd1c1830a
Matías Fabian Sánchez Soto <matias.fss97@gmail.com>
2024-12-03 15:18:15 -0300

    configuración para peticiones HTTP con axios

---
+++ src/api/calendarApi.js
@@ -0,0 +1,14 @@
+import { getEnvVariables } from "../helpers"
+import axios from {axios}
+
+const {VITE_API_URL} = getEnvVariables()
+
+const calendarApi = axios.create({
+    baseURL: VITE_API_URL
+})
+
+
+// Configurar interceptores
+
+
+export default calendarApi
