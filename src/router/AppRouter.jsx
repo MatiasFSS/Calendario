b9be2bfc0e76db88eb87c2a74459d3d97c781132
Matías Fabian Sánchez Soto <matias.fss97@gmail.com>
2024-12-03 15:10:23 -0300

    Creación custom hook useForm, recibir datos de los formularios Login y Registro

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -6,7 +6,7 @@ import { CalendarPage } from "../calendar"
 
 export const AppRouter = () => {
 
-    const authStatus = 'authenticated'
+    const authStatus = 'not-authenticated'
     return (
         <Routes>
             {
